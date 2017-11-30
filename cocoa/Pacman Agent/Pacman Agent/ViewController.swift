d9e21364aba95c471fa2b994b4923c31da7dd5fa
Jack Arendt <jack.arendt93@gmail.com>
2017-11-29 21:39:51 -0800

    Added Xcode project and app icon.

---
+++ cocoa/Pacman Agent/Pacman Agent/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Pacman Agent
+//
+//  Created by Jack Arendt on 11/29/17.
+//  Copyright © 2017 Jack Arendt. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
