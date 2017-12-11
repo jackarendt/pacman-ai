8956db233442024d6940ae5497662bbd78f96e8f
Jack Arendt <jack.arendt93@gmail.com>
2017-12-11 14:38:14 -0800

    Added python files for reading images and processing them to be classified.

---
+++ vision/src/classify_tiles.py
@@ -0,0 +1,5 @@
+"""File that classifies different tiles into their respective groups."""
+
+import numpy
+import os
+import tensorflow as tf
---
+++ vision/src/read_input_data.py
@@ -0,0 +1,74 @@
+"""Reads input data from the tiles and segments it into training and  test sets."""
+
+from math import *
+import itertools
+import os
+import pandas as pd
+from PIL import Image
+
+COLUMNS = ["image", "label"]
+LABEL = "label"
+
+RELATIVE_DATA_DIR = "../data/"
+
+# Indices of different color bands stored in RGBA format.
+RGBA_R = 0
+RGBA_G = 1
+RGBA_B = 2
+RGBA_A = 3
+
+def read_input_data():
+  """
+  Reads the input data and converts it to a 1D array of image data in ARGB format, with the
+  appropriate label.
+  """
+  data_set = pd.read_csv(RELATIVE_DATA_DIR + "labels.csv", skipinitialspace=True,
+                         skiprows=1, names=COLUMNS)
+
+  for image_idx, row in data_set.iterrows():
+    # Load the image, and get the RBGA pixel data.
+    im = Image.open(RELATIVE_DATA_DIR + row[COLUMNS[0]])
+    rgba_pixels = im.getdata()
+
+    # Convert the rgba pixel data to ARGB (the macOS default for screen captures), and flatten it
+    # to a 1D array of pixel values with format [A0, R0, G0, B0, A1, R1, G1, B1...].
+    argb_pixels = \
+        [(pixel[RGBA_A], pixel[RGBA_R], pixel[RGBA_G], pixel[RGBA_B]) for pixel in rgba_pixels]
+    argb_data = [x for sets in argb_pixels for x in sets]
+
+    # Change the image column so that it is a 1D array of pixel values instead of an image name.
+    data_set.at[image_idx, COLUMNS[0]] = argb_data
+
+  # Return the processed data set.
+  return data_set
+
+def split_data_set(data_set, train_percentage=0.64, cv_percentage=0.16, test_percentage=0.2):
+  """ Splits the data set into a training, cross validation, and test training group. """
+
+  original_sum = train_percentage + cv_percentage + test_percentage
+  if original_sum != 1:
+    train_percentage = train_percentage / original_sum
+    cv_percentage = cv_percentage / original_sum
+    test_percentage = test_percentage / original_sum
+    print("Dataset partition ratios do not total 1. They have been normalized to:\n" +
+          "train: %f\ncv: %f\ntest: %f\n" % (train_percentage, cv_percentage, test_percentage))
+
+  data_set = data_set.sample(frac=1).reset_index(drop=True)
+
+  train_idx = floor(train_percentage * len(data_set.index))
+  cv_idx = train_idx + floor(cv_percentage * len(data_set.index))
+
+  train = data_set[0:train_idx].reset_index(drop=True)
+  cv = data_set[train_idx:cv_idx].reset_index(drop=True)
+  test = data_set[cv_idx:].reset_index(drop=True)
+
+  return (train, cv, test)
+
+if __name__ == '__main__':
+  # Reads in the data, splits it into 3 data sets, and prints a description.
+  data = read_input_data()
+  train, cv, test = split_data_set(data, cv_percentage=0)
+
+  print("train", train)
+  print("cv", cv)
+  print("test", test)
